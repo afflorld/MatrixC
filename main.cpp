aa6ebe0d08dec4632c45db3f9c5976315737cc39
afflorld <sl.adamec@gmail.com>
2024-12-01 22:43:17 +0100

    Final Version

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <time.h>
 #include <stdlib.h>
 #include "h/matrixDB.h"
 struct t_matica* prva = NULL;
@@ -176,12 +177,12 @@ void SUM(){
             for(int i = 0; i < riadky; i++){
                 for(int j = 0; j < stlpce; j++){
                     matica[i][j] = odkazV->matica[i][j] + odkazU->matica[i][j];
-                    printf("%d", matica[i][j]);
+                    printf("%d ", matica[i][j]);
                 }
                 printf("\n");
             }
 
-   
+
             save(matica, riadky, stlpce, &prva, poradie);
 
         }
@@ -389,7 +390,7 @@ void TRANS(){
         for(int i = 0; i < riadky; i++){
             for(int j = 0; j < stlpce; j++){
                 matica[i][j] = odkazU->matica[j][i];
-                printf("%d", matica[i][j]);
+                printf("%d ", matica[i][j]);
             }
             printf("\n");
         }
@@ -550,76 +551,72 @@ void MULT_M(){
             return;
         }else{
 
-            printf("Zadaj poradie matice v:");
-            scanf(" %d", &v);
+            do{
+                printf("Zadaj poradie matice v:");
+                scanf(" %d", &v);
+            }while(zakaz[v] != v);
 
-            if(zakaz[v] != v){
-                printf("Zle cislo\n");
-                return;
-            }else{
+            odkazV = prva;
 
-                odkazV = prva;
-
-                while(odkazV != NULL){
-                    if(odkazV->poradie == v){
-                        break;
-                    }
-                    odkazV = odkazV->dalsia;
+            while(odkazV != NULL){
+                if(odkazV->poradie == v){
+                    break;
                 }
-
-
-                riadky = odkazU->riadky;
-                stlpce = odkazV->stlpce;
-
-
-                printf("riadky: %d\n", riadky);
-                printf("stlpce: %d\n", stlpce);
-
-
-                printf("u:\n");
-
-                for(int i = 0; i < odkazU->riadky; i++){
-                    for(int j = 0; j < odkazU->stlpce; j++){
-                        printf("%d ", odkazU->matica[i][j] );
-                    }
-                    printf("\n");
-                }
-
-                printf("v:\n");
-
-                for(int i = 0; i < odkazV->riadky; i++){
-                    for(int j = 0; j < odkazV->stlpce; j++){
-                        printf("%d ", odkazV->matica[i][j] );
-                    }
-                    printf("\n");
-                }
-
-
-                int** matica = (int**)malloc(riadky * sizeof(int*));
-                for(int i = 0; i < riadky; i++){
-                    matica[i] = (int*)malloc(stlpce * sizeof(int));
-                }
-
-                cislo++;
-                poradie = 500 + cislo;
-                printf("Nova matica #%d\n", poradie);
-
-                for(int i = 0; i < riadky; i++){
-                    for(int j = 0; j < stlpce; j++){
-                        for(int k = 0; k < odkazU->stlpce; k++){
-                            matica[i][j] += odkazU->matica[i][k] * odkazV->matica[k][j];
-                        }
-                        printf("%d ", matica[i][j]);
-                    }
-                    printf("\n");
-                }
-
-                save(matica, riadky, stlpce, &prva, poradie);
-
+                odkazV = odkazV->dalsia;
             }
-        }
 
+
+            riadky = odkazU->riadky;
+            stlpce = odkazV->stlpce;
+
+
+            printf("riadky: %d\n", riadky);
+            printf("stlpce: %d\n", stlpce);
+
+
+            printf("u:\n");
+
+            for(int i = 0; i < odkazU->riadky; i++){
+                for(int j = 0; j < odkazU->stlpce; j++){
+                    printf("%d ", odkazU->matica[i][j] );
+                }
+                printf("\n");
+            }
+
+            printf("v:\n");
+
+            for(int i = 0; i < odkazV->riadky; i++){
+                for(int j = 0; j < odkazV->stlpce; j++){
+                    printf("%d ", odkazV->matica[i][j] );
+                }
+                printf("\n");
+            }
+
+
+            int** matica = (int**)malloc(riadky * sizeof(int*));
+            for(int i = 0; i < riadky; i++){
+                matica[i] = (int*)malloc(stlpce * sizeof(int));
+            }
+
+            cislo++;
+            poradie = 500 + cislo;
+            printf("Nova matica #%d\n", poradie);
+
+            for(int i = 0; i < riadky; i++){
+                for(int j = 0; j < stlpce; j++){
+                    for(int k = 0; k < odkazU->stlpce; k++){
+                        matica[i][j] += odkazU->matica[i][k] * odkazV->matica[k][j];
+                    }
+                    printf("%d ", matica[i][j]);
+                }
+                printf("\n");
+            }
+
+            save(matica, riadky, stlpce, &prva, poradie);
+
+        }
     }
+
 
 };
 void INV(){
@@ -637,8 +634,8 @@ void INV(){
     int zakaz[1000]; 
 
     printf("Len pre matice 2x2, 3x3, 4x4, 5x5\n");
-    printf("Vyber si mieru presnosti");
-    scanf("%lf ", &presnost);
+    printf("Vyber si mieru presnosti:");
+    scanf(" %lf", &presnost);
 
     printf("\n");
 
@@ -824,27 +821,28 @@ void INV(){
 
                     for (int i = 0; i < riadky; i++) {
                         for (int j = 0; j < stlpce; j++) {
-                            double minor[3][3];
-                            int minor_row = 0, minor_col;
+                            double mmat[3][3];
+                            int mmat_riadky = 0, mmat_stlpce;
                             for (int r = 0; r < 4; r++) {
                                 if (r == i) continue;
-                                minor_col = 0;
+                                mmat_stlpce = 0;
                                 for (int c = 0; c < 4; c++) {
                                     if (c == j) continue;
-                                    minor[minor_row][minor_col] = mat[r][c];
-                                    minor_col++;
+                                    mmat[mmat_riadky][mmat_stlpce] = mat[r][c];
+                                    mmat_stlpce++;
                                 }
-                                minor_row++;
+                                mmat_riadky++;
                             }
 
-                            double minor_det =
-                                minor[0][0] * (minor[1][1] * minor[2][2] - minor[1][2] * minor[2][1]) -
-                                minor[0][1] * (minor[1][0] * minor[2][2] - minor[1][2] * minor[2][0]) +
-                                minor[0][2] * (minor[1][0] * minor[2][1] - minor[1][1] * minor[2][0]);
+                            double mmat_det =
+                                mmat[0][0] * (mmat[1][1] * mmat[2][2] - mmat[1][2] * mmat[2][1]) -
+                                mmat[0][1] * (mmat[1][0] * mmat[2][2] - mmat[1][2] * mmat[2][0]) +
+                                mmat[0][2] * (mmat[1][0] * mmat[2][1] - mmat[1][1] * mmat[2][0]);
 
-                            inv[j][i] = ((i + j) % 2 == 0 ? 1 : -1) * minor_det;
+                            inv[j][i] = ((i + j) % 2 == 0 ? 1 : -1) * mmat_det;
                         }
                     }
+
 
                     for (int i = 0; i < riadky; i++) {
                         for (int j = 0; j < stlpce; j++) {
@@ -870,50 +868,52 @@ void INV(){
             }
             if(odkazU->riadky == 5 && odkazU->stlpce == 5){
 
-                double cofactors[5][5];
-                double minor[4][4];
+                double nova[5][5];
+                double mmat[4][4];
 
                 for (int i = 0; i < 5; i++) {
                     for (int j = 0; j < 5; j++) {
-                        int minor_row = 0;
+                        int mmat_riadky = 0;
                         for (int r = 0; r < 5; r++) {
                             if (r == i) continue;
-                            int minor_col = 0;
+                            int mmat_stlpce = 0;
                             for (int c = 0; c < 5; c++) {
                                 if (c == j) continue;
-                                minor[minor_row][minor_col] = mat[r][c];
-                                minor_col++;
+                                mmat[mmat_riadky][mmat_stlpce] = mat[r][c];
+                                mmat_stlpce++;
                             }
-                            minor_row++;
+                            mmat_riadky++;
                         }
 
-                        double minor_det = 
-                            minor[0][0] * (minor[1][1] * (minor[2][2] * minor[3][3] - minor[2][3] * minor[3][2])
-                                    - minor[1][2] * (minor[2][1] * minor[3][3] - minor[2][3] * minor[3][1])
-                                    + minor[1][3] * (minor[2][1] * minor[3][2] - minor[2][2] * minor[3][1]))
-                            - minor[0][1] * (minor[1][0] * (minor[2][2] * minor[3][3] - minor[2][3] * minor[3][2])
-                                    - minor[1][2] * (minor[2][0] * minor[3][3] - minor[2][3] * minor[3][0])
-                                    + minor[1][3] * (minor[2][0] * minor[3][2] - minor[2][2] * minor[3][0]))
-                            + minor[0][2] * (minor[1][0] * (minor[2][1] * minor[3][3] - minor[2][3] * minor[3][1])
-                                    - minor[1][1] * (minor[2][0] * minor[3][3] - minor[2][3] * minor[3][0])
-                                    + minor[1][3] * (minor[2][0] * minor[3][1] - minor[2][1] * minor[3][0]))
-                            - minor[0][3] * (minor[1][0] * (minor[2][1] * minor[3][2] - minor[2][2] * minor[3][1])
-                                    - minor[1][1] * (minor[2][0] * minor[3][2] - minor[2][2] * minor[3][0])
-                                    + minor[1][2] * (minor[2][0] * minor[3][1] - minor[2][1] * minor[3][0]));
 
-                        cofactors[i][j] = ((i + j) % 2 == 0 ? 1 : -1) * minor_det;
+                        double mmat_det = 
+                            mmat[0][0] * (mmat[1][1] * (mmat[2][2] * mmat[3][3] - mmat[2][3] * mmat[3][2])
+                                    - mmat[1][2] * (mmat[2][1] * mmat[3][3] - mmat[2][3] * mmat[3][1])
+                                    + mmat[1][3] * (mmat[2][1] * mmat[3][2] - mmat[2][2] * mmat[3][1]))
+                            - mmat[0][1] * (mmat[1][0] * (mmat[2][2] * mmat[3][3] - mmat[2][3] * mmat[3][2])
+                                    - mmat[1][2] * (mmat[2][0] * mmat[3][3] - mmat[2][3] * mmat[3][0])
+                                    + mmat[1][3] * (mmat[2][0] * mmat[3][2] - mmat[2][2] * mmat[3][0]))
+                            + mmat[0][2] * (mmat[1][0] * (mmat[2][1] * mmat[3][3] - mmat[2][3] * mmat[3][1])
+                                    - mmat[1][1] * (mmat[2][0] * mmat[3][3] - mmat[2][3] * mmat[3][0])
+                                    + mmat[1][3] * (mmat[2][0] * mmat[3][1] - mmat[2][1] * mmat[3][0]))
+                            - mmat[0][3] * (mmat[1][0] * (mmat[2][1] * mmat[3][2] - mmat[2][2] * mmat[3][1])
+                                    - mmat[1][1] * (mmat[2][0] * mmat[3][2] - mmat[2][2] * mmat[3][0])
+                                    + mmat[1][2] * (mmat[2][0] * mmat[3][1] - mmat[2][1] * mmat[3][0]));
+
+                        nova[i][j] = ((i + j) % 2 == 0 ? 1 : -1) * mmat_det;
+
                     }
                 }
 
                 for (int i = 0; i < 5; i++) {
                     for (int j = 0; j < 5; j++) {
-                        inv[i][j] = cofactors[j][i];
+                        inv[i][j] = nova[j][i];
                     }
                 }
 
                 det = 0.0;
                 for (int i = 0; i < 5; i++) {
-                    det += mat[0][i] * cofactors[0][i];
+                    det += mat[0][i] * nova[0][i];
                 }
 
                 if (det == 0) {
@@ -1089,50 +1089,52 @@ void DET(){
         }
         if(odkazU->riadky == 5 && odkazU->stlpce == 5){
 
-            double cofactors[5][5];
-            double minor[4][4];
+            double nova[5][5];
+            double mmat[4][4];
 
             for (int i = 0; i < 5; i++) {
                 for (int j = 0; j < 5; j++) {
-                    int minor_row = 0;
+                    int mmat_riadky = 0;
                     for (int r = 0; r < 5; r++) {
                         if (r == i) continue;
-                        int minor_col = 0;
+                        int mmat_stlpce = 0;
                         for (int c = 0; c < 5; c++) {
                             if (c == j) continue;
-                            minor[minor_row][minor_col] = mat[r][c];
-                            minor_col++;
+                            mmat[mmat_riadky][mmat_stlpce] = mat[r][c];
+                            mmat_stlpce++;
                         }
-                        minor_row++;
+                        mmat_riadky++;
                     }
 
-                    double minor_det = 
-                        minor[0][0] * (minor[1][1] * (minor[2][2] * minor[3][3] - minor[2][3] * minor[3][2])
-                                - minor[1][2] * (minor[2][1] * minor[3][3] - minor[2][3] * minor[3][1])
-                                + minor[1][3] * (minor[2][1] * minor[3][2] - minor[2][2] * minor[3][1]))
-                        - minor[0][1] * (minor[1][0] * (minor[2][2] * minor[3][3] - minor[2][3] * minor[3][2])
-                                - minor[1][2] * (minor[2][0] * minor[3][3] - minor[2][3] * minor[3][0])
-                                + minor[1][3] * (minor[2][0] * minor[3][2] - minor[2][2] * minor[3][0]))
-                        + minor[0][2] * (minor[1][0] * (minor[2][1] * minor[3][3] - minor[2][3] * minor[3][1])
-                                - minor[1][1] * (minor[2][0] * minor[3][3] - minor[2][3] * minor[3][0])
-                                + minor[1][3] * (minor[2][0] * minor[3][1] - minor[2][1] * minor[3][0]))
-                        - minor[0][3] * (minor[1][0] * (minor[2][1] * minor[3][2] - minor[2][2] * minor[3][1])
-                                - minor[1][1] * (minor[2][0] * minor[3][2] - minor[2][2] * minor[3][0])
-                                + minor[1][2] * (minor[2][0] * minor[3][1] - minor[2][1] * minor[3][0]));
 
-                    cofactors[i][j] = ((i + j) % 2 == 0 ? 1 : -1) * minor_det;
+                    double mmat_det = 
+                        mmat[0][0] * (mmat[1][1] * (mmat[2][2] * mmat[3][3] - mmat[2][3] * mmat[3][2])
+                                - mmat[1][2] * (mmat[2][1] * mmat[3][3] - mmat[2][3] * mmat[3][1])
+                                + mmat[1][3] * (mmat[2][1] * mmat[3][2] - mmat[2][2] * mmat[3][1]))
+                        - mmat[0][1] * (mmat[1][0] * (mmat[2][2] * mmat[3][3] - mmat[2][3] * mmat[3][2])
+                                - mmat[1][2] * (mmat[2][0] * mmat[3][3] - mmat[2][3] * mmat[3][0])
+                                + mmat[1][3] * (mmat[2][0] * mmat[3][2] - mmat[2][2] * mmat[3][0]))
+                        + mmat[0][2] * (mmat[1][0] * (mmat[2][1] * mmat[3][3] - mmat[2][3] * mmat[3][1])
+                                - mmat[1][1] * (mmat[2][0] * mmat[3][3] - mmat[2][3] * mmat[3][0])
+                                + mmat[1][3] * (mmat[2][0] * mmat[3][1] - mmat[2][1] * mmat[3][0]))
+                        - mmat[0][3] * (mmat[1][0] * (mmat[2][1] * mmat[3][2] - mmat[2][2] * mmat[3][1])
+                                - mmat[1][1] * (mmat[2][0] * mmat[3][2] - mmat[2][2] * mmat[3][0])
+                                + mmat[1][2] * (mmat[2][0] * mmat[3][1] - mmat[2][1] * mmat[3][0]));
+
+                    nova[i][j] = ((i + j) % 2 == 0 ? 1 : -1) * mmat_det;
+
                 }
             }
 
             for (int i = 0; i < 5; i++) {
                 for (int j = 0; j < 5; j++) {
-                    inv[i][j] = cofactors[j][i];
+                    inv[i][j] = nova[j][i];
                 }
             }
 
             det = 0.0;
             for (int i = 0; i < 5; i++) {
-                det += mat[0][i] * cofactors[0][i];
+                det += mat[0][i] * nova[0][i];
             }
 
             if (det == 0) {
@@ -1148,19 +1150,26 @@ void DET(){
 };
 
 void SAVE(){
-   
+
     static int cislo = 0;
     static int poradie;
     struct t_matica* odkaz = prva;
+    time_t t = time(NULL);
+    struct tm *tm = localtime(&t);
 
     poradie = cislo;
     FILE *fp = fopen("./history.txt", "a");
 
-    fprintf(fp, "Historia ulozenia %d\n", poradie);
+    fprintf(fp, "Historia ulozenia: %02d-%02d-%04d\n", tm->tm_mday, tm->tm_mon + 1, tm->tm_year + 1900);
 
     while (odkaz != NULL) {
         fprintf(fp, "Matica #%d (%d x %d)\n", odkaz->poradie, odkaz->riadky, odkaz->stlpce);
-        //doplnit loopu
+        for(int i = 0; i < odkaz->riadky; i++){
+            for(int j = 0; j < odkaz->stlpce; j++){
+                fprintf(fp, "%d ", odkaz->matica[i][j] );
+            }
+            fprintf(fp, "\n");
+        } 
         odkaz = odkaz->dalsia;
     }
 
